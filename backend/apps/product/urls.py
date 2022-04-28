f4407e1c36697562a261eff2a735aea05e3764bb
daiyr isaev <96345264+daiyrisaev@users.noreply.github.com>
2022-04-28 19:12:18 +0300

    add register commit

--- backend/apps/product/urls.py
+++ backend/apps/product/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('',IndexPage.as_view(),name='index'),
     path('getSubcategory/', get_subcategory, name='get_subcategory')
 ]
--- backend/apps/product/views.py
+++ backend/apps/product/views.py
@@ -3,6 +3,7 @@ import json
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import SubCategory
+from django.views.generic import TemplateView
 
 
 def get_subcategory(request):
@@ -10,3 +11,7 @@ def get_subcategory(request):
     result = list(SubCategory.objects.filter(
     category_id=int(id)).values('id', 'name'))
     return HttpResponse(json.dumps(result), content_type="application/json")
+
+
+class IndexPage(TemplateView):
+    template_name = "index.html"
